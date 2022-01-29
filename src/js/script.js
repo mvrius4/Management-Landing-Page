73e30719660b23677580bf50b0112073e03602a6
mvrius69 <xmariusro@gmail.com>
2022-01-29 14:15:50 +0200

    added countup

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,6 @@
+import { CountUp } from 'countup.js';
+import { gsap, ScrollTrigger, Draggable, MotionPathPlugin } from 'gsap/all';
+
 const nav = document.querySelector("nav");
 const navOpener = document.querySelector("#nav-open");
 const navCloser = document.querySelector("#nav-close");
@@ -11,3 +14,11 @@ navCloser.addEventListener("click", () => {
     if(nav.classList === "") return;
     nav.classList.remove("active");
 })
+
+const options = { duration: 5 };
+let countOne = new CountUp('count-one', 1351, options);
+let countTwo = new CountUp('count-two', 24, options);
+let countThree = new CountUp('count-three', 29, options);
+countOne.start()
+countTwo.start()
+countThree.start()
