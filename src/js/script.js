c3e0517187c7e5035c1e66b491f97b6ec294f894
mvrius69 <xmariusro@gmail.com>
2022-03-06 20:56:18 +0200

    last changes - done

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,3 @@
-import { CountUp } from 'countup.js';
-
 const nav = document.querySelector("nav");
 const navOpener = document.querySelector("#nav-open");
 const navCloser = document.querySelector("#nav-close");
@@ -13,11 +11,3 @@ navCloser.addEventListener("click", () => {
     if(nav.classList === "") return;
     nav.classList.remove("active");
 })
-
-const options = { duration: 5 };
-let countOne = new CountUp('count-one', 1351, options);
-let countTwo = new CountUp('count-two', 24, options);
-let countThree = new CountUp('count-three', 29, options);
-countOne.start()
-countTwo.start()
-countThree.start()
