4c163133bf7b9120698dc080b8632d5c3239df58
mvrius69 <xmariusro@gmail.com>
2022-03-15 12:55:16 +0200

    added scroll reveal animation

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,9 @@
 const nav = document.querySelector("nav");
 const navOpener = document.querySelector("#nav-open");
 const navCloser = document.querySelector("#nav-close");
+const reveals = document.querySelectorAll('.reveal');
+const windowheight = window.innerHeight;
+const revealpoint = 150;
 
 navOpener.addEventListener("click", () => {
     if(nav.classList === "active") return;
@@ -11,3 +14,12 @@ navCloser.addEventListener("click", () => {
     if(nav.classList === "") return;
     nav.classList.remove("active");
 })
+
+window.addEventListener('scroll', () => {
+    for(let i = 0; i < reveals.length; i++) {
+        let revealtop = reveals[i].getBoundingClientRect().top;
+        if(revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add('active');
+        }
+    }
+});
